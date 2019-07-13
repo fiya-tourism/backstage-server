a363d5e47714d09b0d84c3d1385ba4b69c92458a
wuyu <812101309@qq.com>
2019-07-13 15:06:43 +0800

    修改包结构

---
+++ src/main/java/com/fy/backstage/config/DefaultView.java
@@ -0,0 +1,24 @@
+package com.fy.backstage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+ 
+/**
+ * 系统默认界面设置
+ *
+ * @auther hzy
+ * @data 2018/4/28 9:59
+ * @QQ 291471000
+ */
+@Configuration
+public class DefaultView extends WebMvcConfigurerAdapter {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // index 在 templates 文件夹下
+        registry.addViewController("/").setViewName("backstage/commons/content");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        super.addViewControllers(registry);
+    }
+}
