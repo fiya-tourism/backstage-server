9991fa276454597c33561cf8b0d6dfa28ef65b7d
wuyu <812101309@qq.com>
2019-07-16 14:48:56 +0800

    提交server

--- src/main/java/com/fy/backstage/user/service/UserService.java
+++ src/main/java/com/fy/backstage/user/service/UserService.java
@@ -6,13 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
-
-
 @FeignClient(value = "user-server")
 public interface UserService {
     @RequestMapping(value="queryUserList",method = RequestMethod.GET)
     List<UserVO> queryUserList();
-
-
-
 }
